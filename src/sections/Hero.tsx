fb06cc593d2193f1412ce921d3a2970ee01bbead
Alianderson <99621023+killozila@users.noreply.github.com>
2025-06-23 07:23:49 -0300

    Initial commit

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,54 @@
+'use client';
+import { useRef } from 'react';
+import { Button } from '@/components/Button';
+import starsBg from '@/assets/stars.png';
+import { motion, useScroll, useTransform } from 'framer-motion';
+
+export const Hero = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ['start end', 'end start']
+  });
+
+  const backgroundPositionY = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [-300, 300]
+  );
+
+  return (
+    <motion.section
+      ref={sectionRef}
+      className='h-[492px] md:h-[800px] flex items-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)] w-full'
+      style={{
+        backgroundImage: `url(${starsBg.src})`,
+        backgroundPositionY,
+      }}
+      animate={{
+        backgroundPositionX: starsBg.width,
+      }}
+      transition={{
+        repeat: Infinity,
+        ease: "linear",
+        duration: 120,
+      }}
+    >
+      <div className="absolute inset-0 bg-[radial-gradient(75%_75%_at_center_center,rgb(140,69,255,.5)_15%,rgb(14,0,36,.5)_78%,transparent)]"></div>
+      <div className="absolute h-64 w-64 md:h-96 md:w-96 bg-purple-500 rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,.1),0_0_50px_rgb(140,69,255)]"></div>
+      <div className="absolute h-[344px] w-[344px] md:h-[500px] md:w-[500px] border border-white opacity-20 rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+
+      <div className="relative w-full px-4 md:px-8 mt-16">
+        <h1 className='text-8xl font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] bg-clip-text text-transparent text-center'>
+          AI SEO
+        </h1>
+        <p className='text-lg text-white/70 mt-5 text-center max-w-xl mx-auto'>
+          eleve suas vendas com top 0.1% sale page, incluindo milhares de estratégias de conversão.
+        </p>
+        <div className="flex justify-center mt-5">
+          <Button>Join Waitlist</Button>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
