3d54bfa898d7cc54179bba88f9cc5e56a13eaa76
Alianderson "World <118865435+alianderson-world@users.noreply.github.com>
2025-06-23 07:49:47 -0300

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-export const Button = (props: React.PropsWidthChildren) => {
+export const Button = (props: React.PropsWithChildren) => {
     return (
         <button className='relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]'>
             <div className="absolute inset-0">
